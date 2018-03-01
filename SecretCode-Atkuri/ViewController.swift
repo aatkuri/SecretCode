ac446581d8011d18547f0074c3e60111ef875524
student <s530458@ol10019m.nwmissouri.edu>
2018-03-01 15:38:30 -0600

    Initial Commit

---
+++ SecretCode-Atkuri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretCode-Atkuri
+//
+//  Created by Atkuri,Ashok on 3/1/18.
+//  Copyright © 2018 Atkuri,Ashok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
