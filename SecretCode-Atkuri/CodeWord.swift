6a85e30a9ae119ecf791db9aa25f92a81a061063
student <s530458@ol10019m.nwmissouri.edu>
2018-03-01 19:42:48 -0600

    Initial Complete Version

--- SecretCode-Atkuri/CodeWord.swift
+++ SecretCode-Atkuri/CodeWord.swift
@@ -10,39 +10,33 @@ import Foundation
 
 
 class CodeWord{
-    // a. Symbols is an array of strings and will be the choices for the TableView
+    
     private var _symbols:[String] = []
-    // b. Count is the number of symbols in the code and guess.
     private var _count:Int
-    // c. Code is an array of strings and is the secret code
     private var _code:[String] = []
-    // d. Guess is an array of strings and holds the current guess.
     private var _guess:[String] = []
-    // e. OnSymbol gives the index of the next place to add a symbol in the guess
     private var _OnSymbol:Int = 0
-    //f. Attempts is the number of times that a guess has been made.
     private var _attempts:Int = 1
-    // g. Status is the status message for the game.
     private var _status:String
     
+    var symbols:[String] {
+        get { return _symbols }
+        set { _symbols = newValue }
+    }
     var count:Int { get{ return _count } }
     var code:[String] { get{ return _code } }
     var guess:[String] { get{ return _guess } }
     var onSymbol:Int { get{ return _OnSymbol } }
     var attempts:Int { get{ return _attempts } }
     
-    private static var inst:CodeWord = CodeWord(4)
-    
-    /* h. It should have a single initializer that takes in the count for the size of  the code. It will initialize the array of symbols (There should be at least 4. It will create the secret code. Your project should work for a code length of 4 or larger. It will initialize the other attributes as needed. */
     private init(_ codeSize:Int){
         _count = codeSize
         _status = "Attempt \(_attempts): \(_guess.count) symbols guessed"
-        setSymbols(uniCodeStartingChar: "A", noOfSymbols: 3, offSet: 1)
+        _symbols = ["A","B","C","D"]
         _code = self.generateCodeWord()
     }
     
-    /* i. It should have a method to add a symbol to the guess. Increase onSymbol by one. Set the status message to "Attempt X: X symbols guessed". If we filled the last position, reset onSymbol to zero. Count the number of correct positions. Set the status message to "Guess completed: x correct" */
-    func addSymbolToGuess(guessSymbol:String){
+     func addSymbolToGuess(guessSymbol:String){
         if _OnSymbol < _count-1 {
             _guess.append(guessSymbol)
             _OnSymbol += 1
@@ -63,14 +57,10 @@ class CodeWord{
                     _status = "Unbroken Code – Press reset to start again."
                     
                 }
-            }
-            setNumericStatusMsg()
+            }            
         }
-        print(_guess)
-        print(_status)
     }
     
-    //j. It should have a method that returns the number of places where the guess matches the code.
     func matchedSymbols() -> Int {
         var correct = 0;
         for i in 0 ..< _guess.count{
@@ -81,30 +71,10 @@ class CodeWord{
         return correct
     }
     
-    //k. It should have a method that returns a status message for the guess.
     func getStatusMessage() ->String {
         return _status
     }
-    func increaseCodeSize(){
-        _count += 1
-    }
-    private func setNumericStatusMsg(){
-        if _symbols.count>0 && (Int)(UnicodeScalar(_symbols[0])!.value) >= 48
-            && (Int)(UnicodeScalar(_symbols[_symbols.count-1])!.value) <= 57 {
-            var sum = 0
-            for i in 0 ..< _guess.count {
-                let temp = Int(_guess[i])! - Int(_code[i])!
-                if temp < 0 {
-                    sum += temp * -1
-                } else {
-                    sum += temp
-                }
-            }
-            _status += "\nSum of differences is \(sum)"
-        }
-    }
     
-    //l. It should have a method that returns the current guess
     func currentGuess() -> String {
         var guess = ""
         for i in 0 ..< _guess.count {
@@ -113,19 +83,16 @@ class CodeWord{
         for _ in _guess.count ..< _count {
             guess += "  -"
         }
-        print(guess)
         return guess.trimmingCharacters(in: NSCharacterSet.whitespaces)
     }
     
-    //m. It should have a method that returns true if the guess matches the code.
     func correctGuess() -> Bool {
         return matchedSymbols()==_count
     }
     
-    //n. It should have a reset method that assigns different symbols to the code and sets the onLetter to zero.
     func reset(){
         _code = generateCodeWord()
-        resetGuess()
+        _guess = []
         _OnSymbol = 0
         _attempts = 1
         _status = "Attempt \(_attempts): \(_guess.count) symbols guessed"
@@ -135,17 +102,9 @@ class CodeWord{
         _guess = []
     }
     
+    private static let codemodel:CodeWord = CodeWord(4)
     static func getInstance() -> CodeWord{
-        return inst
-    }
-    
-    func setSymbols(uniCodeStartingChar:String, noOfSymbols:Int, offSet:Int){
-        let startIdx:Int = (Int)(UnicodeScalar(uniCodeStartingChar)!.value)
-        _symbols = []
-        for i in stride(from:startIdx, through:startIdx+(noOfSymbols*offSet), by :offSet) {
-            _symbols.append("\(UnicodeScalar(i) ?? "A")")
-        }
-        reset()
+        return codemodel
     }
     
     func getSymbols() -> [String] {
@@ -160,6 +119,7 @@ class CodeWord{
         if _guess.count != 0 {
             _guess.removeLast()
             _OnSymbol -= 1
+            _attempts -= 1
             _status = "Attempt \(_attempts): \(_guess.count) symbols guessed"
         }
     }
@@ -167,10 +127,10 @@ class CodeWord{
     func hint() -> String {
         for i in 0 ..< _symbols.count {
             if _symbols[i] == _code[_OnSymbol] {
-                return "I am at position \(i+1) in symbol list."
+                return "I am at position \(i+1)."
             }
         }
-        return "Sorry, I can't help you anymore."
+        return "Sorry, I am out of hints."
     }
     
     private func generateCodeWord() -> [String] {
@@ -179,8 +139,6 @@ class CodeWord{
             let tem:Int = Int(arc4random_uniform(UInt32(_symbols.count)))
             codeWord.append(_symbols[tem])
         }
-        print("Symbols are :",_symbols)
-        print("Code is :",codeWord)
         return codeWord
     }
 }
--- SecretCode-Atkuri/ViewController.swift
+++ SecretCode-Atkuri/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
     @IBOutlet weak var TVSymbols: UITableView!
     @IBOutlet weak var LBLStatus: UILabel!
     @IBOutlet weak var LBLGuess: UILabel!
@@ -19,17 +19,81 @@ class ViewController: UIViewController {
     @IBOutlet weak var BTNHint: UIButton!
     @IBOutlet weak var BTNUndo: UIButton!   
     
+    private var symbols:[String] = AppDelegate.codeWordInstance.getSymbols()
     
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        TVSymbols.delegate = self // our tableView will use this class for both its delegate & data source
+        TVSymbols.dataSource = self
+        LBLPreviousGuess.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AppDelegate.codeWordInstance.getSymbols().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Symbols")! // in lieu of UITableViewCell()
+        cell.textLabel?.text = AppDelegate.codeWordInstance.getSymbols()[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Select..."
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        LBLHint.text = ""
+        AppDelegate.codeWordInstance.addSymbolToGuess(guessSymbol: symbols[indexPath.row])
+        LBLGuess.text = "The Guess : \(AppDelegate.codeWordInstance.currentGuess())"
+        print(LBLGuess.text!)
+        LBLStatus.text = AppDelegate.codeWordInstance.getStatusMessage()
+        print(LBLStatus.text!)
+        if AppDelegate.codeWordInstance.correctGuess() {
+            LBLHint.isEnabled = false
+            BTNUndo.isEnabled = false
+            LBLHint.text = "Congrats, reset and guess new code."
+        } else {
+            BTNHint.isEnabled = true
+            if AppDelegate.codeWordInstance.onSymbol == 0  {
+                BTNUndo.isEnabled = false
+                LBLPreviousGuess.text = LBLPreviousGuess.text! +
+                    "\nGuess \(AppDelegate.codeWordInstance.attempts-1): " + AppDelegate.codeWordInstance.currentGuess()
+                AppDelegate.codeWordInstance.resetGuess()
+            } else if AppDelegate.codeWordInstance.onSymbol == AppDelegate.codeWordInstance.count {
+                BTNUndo.isEnabled = false
+                BTNHint.isEnabled = false
+            }else {
+                BTNUndo.isEnabled = true
+            }
+        }
+    }
 
-
+    @IBAction func ResetOnClick(_ sender: Any) {
+        AppDelegate.codeWordInstance.reset()
+        LBLGuess.text = ""
+        LBLStatus.text = ""
+        LBLPreviousGuess.text = ""
+        LBLHint.text = ""
+        BTNUndo.isEnabled = false
+    }
+    
+    @IBAction func HintOnclick(_ sender: Any) {
+        LBLHint.text = AppDelegate.codeWordInstance.hint()
+    }
+    
+    @IBAction func UndoOnclick(_ sender: Any) {
+        AppDelegate.codeWordInstance.undoLastGuess()
+        if AppDelegate.codeWordInstance.onSymbol == 0 {
+            BTNUndo.isEnabled = false
+        }
+        LBLGuess.text = "The Guess : " + AppDelegate.codeWordInstance.currentGuess()
+        LBLStatus.text = AppDelegate.codeWordInstance.getStatusMessage()
+    } 
 }
 
